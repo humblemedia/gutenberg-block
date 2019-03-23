af566eb0d884efaa9dea9ca1e275fb38b265311b
andrewsplice <andrew.fraser@splicedigital.com>
2019-03-23 16:03:51 -0400

    first stab at column blocks

--- public/wp-content/plugins/my-block/src/block/block.js
+++ public/wp-content/plugins/my-block/src/block/block.js
@@ -12,29 +12,34 @@ import './editor.scss';
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
-registerBlockType( 'cgb/block-my-block', {
+const {
+	RichText
+} = wp.editor;
+
+let registerBlockType1 = registerBlockType( 'my-block/bootstrap-columns', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'my-block - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	title: __( 'Bootstrap columns' ), // Block title.
+	icon: 'editor-table', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
-		__( 'my-block — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
+		__( 'Columns' ),
+		__( 'Bootstrap' ),
 	],
+	attributes:
+	{
+		leftCol: {
+			type: 'string',
+			source: 'html',
+			selector: 'div',
+			default: 'Left column text'
+		},
+		rightCol: {
+			type: 'string',
+			source: 'html',
+			selector: 'div',
+			default: 'Right column text'
+		}
+	},
 
 	/**
 	 * The edit function describes the structure of your block in the context of the editor.
@@ -44,25 +49,35 @@ registerBlockType( 'cgb/block-my-block', {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
-	edit: function( props ) {
-		// Creates a <p class='wp-block-cgb-block-my-block'></p>.
+
+	edit(props) {
+
+		const { setAttributes, attributes } = props;
+
+		const onChangeLeft = value => {
+			setAttributes( { leftCol: value } );
+		};
+		const onChangeRight = value => {
+			setAttributes( { rightCol: value } );
+		};
+
+
 		return (
 			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>my-block</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+			<RichText
+		tagName="div"
+		value={attributes.leftCol}
+		onChange={onChangeLeft}
+			/>
+			<RichText
+		tagName="div"
+		value={attributes.rightCol}
+		onChange={onChangeRight}
+			/>
 			</div>
-		);
+	);
 	},
+
 
 	/**
 	 * The save function defines the way in which the different attributes should be combined
@@ -72,22 +87,26 @@ registerBlockType( 'cgb/block-my-block', {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
-	save: function( props ) {
+	save: function(props) {
+
+		const { attributes } = props;
+
 		return (
-			<div>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>my-block</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+			<div className={ attributes.className }>
+				<RichText.Content
+					tagName="div"
+					className="col-md-6"
+					value={ attributes.leftCol }
+				/>
+				<RichText.Content
+					tagName="div"
+					className="col-md-6"
+					value={ attributes.rightCol }
+				/>
 			</div>
-		);
-	},
+	);
+
+
+
+	}
 } );
